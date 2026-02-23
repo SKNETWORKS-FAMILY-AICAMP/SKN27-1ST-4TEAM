650f392805e01ef204505d9a4ca42bf4ba23d495
ChangJe-P <ckdwpzzz@gmail.com>
2026-02-23 19:46:04 +0900

    main  페이지 오타 수정

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 # .
 st.title('4조 화이팅')
-st.page_link("./pages/faq.py", label="FAQ 페이지", icon="👍")
-st.page_link("./pages/registration.py", label="등록현황 페이지", icon="🚗")
-st.page_link("./pages/parking.py", label="주차장 페이지", icon="🚓")
+st.page_link("./pages/02_faq.py", label="FAQ 페이지", icon="👍")
+st.page_link("./pages/01_registration.py", label="등록현황 페이지", icon="🚗")
+st.page_link("./pages/03_parking.py", label="주차장 페이지", icon="🚓")
 
