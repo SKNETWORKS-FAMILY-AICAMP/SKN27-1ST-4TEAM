73817c848751d2514b94dcef486e10bc7b850fa6
ChangJe-P <ckdwpzzz@gmail.com>
2026-02-23 19:34:32 +0900

    main 페이지 푸쉬

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+# .
 st.title('4조 화이팅')
 st.page_link("./pages/faq.py", label="FAQ 페이지", icon="👍")
 st.page_link("./pages/registration.py", label="등록현황 페이지", icon="🚗")
