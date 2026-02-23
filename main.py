ef81a98fbe6bda9c6679a071733a0008c895f59b
ChangJe-P <ckdwpzzz@gmail.com>
2026-02-23 19:31:58 +0900

    main 페이지 commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('4조 화이팅')
+st.page_link("./pages/faq.py", label="FAQ 페이지", icon="👍")
+st.page_link("./pages/registration.py", label="등록현황 페이지", icon="🚗")
+st.page_link("./pages/parking.py", label="주차장 페이지", icon="🚓")
+
