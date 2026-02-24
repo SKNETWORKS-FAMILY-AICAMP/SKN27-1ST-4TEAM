798846ec6534f41e5b2447f9c147a2337a1e687f
ChangJe-P <ckdwpzzz@gmail.com>
2026-02-24 11:59:50 +0900

    메인 화면 개발로인해 기존 브렌치 수정

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-# .
-st.title('4조 화이팅')
-st.page_link("./pages/02_faq.py", label="FAQ 페이지", icon="👍")
-st.page_link("./pages/01_registration.py", label="등록현황 페이지", icon="🚗")
-st.page_link("./pages/03_parking.py", label="주차장 페이지", icon="🚓")
-
