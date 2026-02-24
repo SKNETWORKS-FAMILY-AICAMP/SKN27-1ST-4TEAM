307afdd778daf9edc52584de62004d70a6e9aaf9
mesrwi-arch <mesrwikk@gmail.com>
2026-02-24 12:24:31 +0900

    1. gitignore 업데이트: __pycache__ 추가 2. sidemenu를 포함한 메인 화면 구축 3. 상세 페이지 개발을 위해 registration.py, faq.py, parking.py 생성

---
+++ pages/02_faq_crawling.py
@@ -0,0 +1,14 @@
+import os
+import sys
+# 현재 파일의 위치를 기준으로 프로젝트 루트(상위 폴더)를 파이썬 경로에 추가
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+import streamlit as st
+
+from common.sidemenu import display_sidebar
+
+st.set_page_config(page_title=" FAQ 수집기", page_icon="🚗", layout="wide") # 넓게 보기 옵션 추가
+display_sidebar() # 공통 사이드바 호출
+
+# --- Streamlit UI 구성 ---
+st.title("🚗 FAQ 크롤러")
